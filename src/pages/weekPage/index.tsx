ee151e9e9a55023190bace8613230ea92138f4f5
Rikuto <orikutobasuke@gmail.com>
2024-01-05 20:01:58 +0900

    1週間登録画面9割完成

--- src/pages/weekPage/index.tsx
+++ src/pages/weekPage/index.tsx
@@ -11,18 +11,30 @@ export default function WeekPage() {
         <>
             <div className={style.wrap}>
                 <div className={style.linkBox}>
-                    <Link href={''} className={style.link}> ＜ 戻る</Link>
+                    <Link href={''} className={style.link}> ＜戻る</Link>
                 </div>
                 <div className={style.listBox}>
                     {week.map((v, idx) =>
-                        <div>
+                        <div key={idx} className={style.dayList}>
                             <div className={style.day}>
-                                <p className={style.dayText}>{ }</p>
+                                <p
+                                    style={v === 0 ? { backgroundColor: '#A64646' } : v === 6 ? { backgroundColor: '#4655A6' } : { backgroundColor: '#46A667' }}
+                                    className={style.dayText}
+                                >{
+                                        v === 0 ? '日' :
+                                            v === 1 ? '月' :
+                                                v === 2 ? '火' :
+                                                    v === 3 ? '水' :
+                                                        v === 4 ? '木' :
+                                                            v === 5 ? '金' :
+                                                                v === 6 ? '土' : undefined
+                                    }</p>
                             </div>
                             <div className={style.home}>
-                                <p></p>
-                                <span></span>
-                                <p></p>
+                                <p>{'大阪梅田'}</p>
+                                <span>←</span>
+                                <p>{'生駒'}</p>
+                                <span>＞</span>
                             </div>
                         </div>
                     )}

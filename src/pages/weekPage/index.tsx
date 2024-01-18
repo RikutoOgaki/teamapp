3e74e20908271e478ed980a1abd10e22d450e19a
Rikuto <orikutobasuke@gmail.com>
2024-01-18 13:54:00 +0900

    １週間の登録データ

--- src/pages/weekPage/index.tsx
+++ src/pages/weekPage/index.tsx
@@ -1,5 +1,6 @@
 import style from '@/styles/weekPage/index.module.scss'
 import Link from 'next/link'
+import { WeekData } from '@/types/WeekData'
 
 export default function WeekPage() {
 
@@ -14,26 +15,26 @@ export default function WeekPage() {
                     <Link href={''} className={style.link}> ＜戻る</Link>
                 </div>
                 <div className={style.listBox}>
-                    {week.map((v, idx) =>
+                    {WeekData.map((v, idx) =>
                         <div key={idx} className={style.dayList}>
                             <div className={style.day}>
                                 <p
-                                    style={v === 0 ? { backgroundColor: '#A64646' } : v === 6 ? { backgroundColor: '#4655A6' } : { backgroundColor: '#46A667' }}
+                                    style={v.weekNum === 0 ? { backgroundColor: '#A64646' } : v.weekNum === 6 ? { backgroundColor: '#4655A6' } : { backgroundColor: '#46A667' }}
                                     className={style.dayText}
                                 >{
-                                        v === 0 ? '日' :
-                                            v === 1 ? '月' :
-                                                v === 2 ? '火' :
-                                                    v === 3 ? '水' :
-                                                        v === 4 ? '木' :
-                                                            v === 5 ? '金' :
-                                                                v === 6 ? '土' : undefined
+                                        v.weekNum === 0 ? '日' :
+                                            v.weekNum === 1 ? '月' :
+                                                v.weekNum === 2 ? '火' :
+                                                    v.weekNum === 3 ? '水' :
+                                                        v.weekNum === 4 ? '木' :
+                                                            v.weekNum === 5 ? '金' :
+                                                                v.weekNum === 6 ? '土' : undefined
                                     }</p>
                             </div>
                             <div className={style.home}>
-                                <p>{'大阪梅田'}</p>
+                                <p>{v.startHome}</p>
                                 <span>←</span>
-                                <p>{'生駒'}</p>
+                                <p>{v.endHome}</p>
                                 <span>＞</span>
                             </div>
                         </div>
---
+++ src/types/WeekData.ts
@@ -0,0 +1,46 @@
+// １週間のデータ
+
+export const WeekData = [
+    {
+        id: 1,
+        weekNum: 0,
+        startHome: '',
+        endHome: ''
+    },
+    {
+        id: 2,
+        weekNum: 1,
+        startHome: '大阪梅田',
+        endHome: '十三'
+    },
+    {
+        id: 3,
+        weekNum: 2,
+        startHome: '大阪梅田',
+        endHome: '十三'
+    },
+    {
+        id: 4,
+        weekNum: 3,
+        startHome: '大阪梅田',
+        endHome: '十三'
+    },
+    {
+        id: 5,
+        weekNum: 4,
+        startHome: '大阪梅田',
+        endHome: '十三'
+    },
+    {
+        id: 6,
+        weekNum: 5,
+        startHome: '大阪梅田',
+        endHome: '十三'
+    },
+    {
+        id: 7,
+        weekNum: 6,
+        startHome: '',
+        endHome: ''
+    },
+]
